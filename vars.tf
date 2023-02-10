3d4058270930c77223b5ee8b36d31ab76aa5ac54
raviteja <ravitejatfcdemo@gmail.com>
2023-02-10 18:08:44 +0530

    Terraform

---
+++ vars.tf
@@ -0,0 +1,4 @@
+variable "cidr_block" {}
+# we are going to take a variable and give it
+# we have to send this cidr_block to roboshop-infra, main.tf
+# we are trying to maintain the uniformity of the variables also
