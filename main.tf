46bcd919767814395250ef5e57099c76f95eecc3
raviteja <ravitejatfcdemo@gmail.com>
2023-02-11 14:52:28 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -16,6 +16,17 @@ resource "aws_subnet" "main" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-subnet-${count.index+1}" }
+    { Name = "${var.env}-subnet-${count.index + 1}" }
   )
 }
+
+
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = "vpc-0380e34c4b82831a1"
+  vpc_id        = aws_vpc.main.id
+}
+# we are connecting both vpc's, one is workstation and another newly created vpc
+# instead of hardcode owner id, we can get information from data.tf using aws_caller_identity module
+# peer_owner_id - to which account we need to connect, my account or other account
+# peer_vpc_id is default vpc id, vpc_id is new vpc created, connect both to current account
