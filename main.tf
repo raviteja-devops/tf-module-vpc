9f30691f53bb423914427cdea195356ffb46247d
raviteja <ravitejatfcdemo@gmail.com>
2023-02-11 11:42:59 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,18 @@ resource "aws_vpc" "main" {
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-vpc" }
-    )
+  )
 }
 # merge function to merge both tags
 # for every resource main tags remain itself, only name tag we add it from resource and merge those things
+
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.cidr_block
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-subnet" }
+  )
+}
