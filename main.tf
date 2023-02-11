b66e95067788674cd3b5bd632d9eba7ce43b0c23
raviteja <ravitejatfcdemo@gmail.com>
2023-02-11 14:58:14 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = "vpc-0380e34c4b82831a1"
   vpc_id        = aws_vpc.main.id
+  auto_accept   = true
 }
 # we are connecting both vpc's, one is workstation and another newly created vpc
 # instead of hardcode owner id, we can get information from data.tf using aws_caller_identity module
