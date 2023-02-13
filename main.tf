03396b835efaca7724439802857f319adcd3f5b4
raviteja <ravitejatfcdemo@gmail.com>
2023-02-13 11:58:27 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -51,3 +51,12 @@ resource "aws_route" "default-vpc" {
   destination_cidr_block = var.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-igw" }
+  )
+}
