c9610997679eecbdb377a2c83a5ae3f8179dfe6c
raviteja <ravitejatfcdemo@gmail.com>
2023-02-13 16:12:05 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -113,3 +113,31 @@ resource "aws_nat_gateway" "ngw" {
   depends_on = [aws_internet_gateway.igw]
 }
 # creating nat-gateway
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = data.aws_vpc.default.cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-private-routetable" }
+  )
+}
+# creating private route table, so to point the natgateway to private subnets
+
+
+resource "aws_route_table_association" "private-rt-assoc" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private.*.id[count.index]
+  route_table_id = aws_route_table.private.id
+}
