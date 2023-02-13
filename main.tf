d61ce9341146b11abc565c659bdd4bedbc2f18de
raviteja <ravitejatfcdemo@gmail.com>
2023-02-13 14:49:42 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -9,14 +9,25 @@ resource "aws_vpc" "main" {
 # for every resource main tags remain itself, only name tag we add it from resource and merge those things
 
 
-resource "aws_subnet" "main" {
-  count = length(var.subnets_cidr)
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnets_cidr[count.index]
+  cidr_block = var.public_subnets_cidr[count.index]
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-subnet-${count.index + 1}" }
+    { Name = "${var.env}-public-subnet-${count.index + 1}" }
+  )
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnets_cidr[count.index]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-private-subnet-${count.index + 1}" }
   )
 }
 
@@ -37,20 +48,13 @@ resource "aws_vpc_peering_connection" "peer" {
 # peer_vpc_id is default vpc id, vpc_id is new vpc created, connect both to current account
 # default_vpc_id is coming from main.tfvars, we don't hard code
 
-resource "aws_route" "default" {
-  route_table_id = aws_vpc.main.default_route_table_id
-  destination_cidr_block = "172.31.0.0/16"
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+
+
 # we allowed traffic from main vpc to default vpc, but TRAFFIC FROM default vpc is not allowed TO MAIN VPC
 # we need to open traffic from both sides
 
-# we get data of defailt vpc in data.tf
-resource "aws_route" "default-vpc" {
-  route_table_id = data.aws_vpc.default.main_route_table_id
-  destination_cidr_block = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+# we get data of default vpc in data.tf
+
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
