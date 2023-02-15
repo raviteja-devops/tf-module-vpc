5b6b9c6552d50f99b1015cdcbd78eba9341e38c0
raviteja <ravitejatfcdemo@gmail.com>
2023-02-15 14:12:19 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -9,32 +9,6 @@ resource "aws_vpc" "main" {
 # for every resource main tags remain itself, only name tag we add it from resource and merge those things
 
 
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnets_cidr[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-public-subnet-${count.index + 1}" }
-  )
-}
-
-
-resource "aws_subnet" "private" {
-  count = length(var.private_subnets_cidr)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnets_cidr[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-private-subnet-${count.index + 1}" }
-  )
-}
-
-
 resource "aws_vpc_peering_connection" "peer" {
   peer_owner_id = data.aws_caller_identity.current.account_id
   peer_vpc_id   = var.default_vpc_id
@@ -62,89 +36,24 @@ resource "aws_internet_gateway" "igw" {
 }
 
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-# IGW cidr
-
-  route {
-    cidr_block = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  }
-# peering connection cidr, peering with workstation vpc
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-public-routetable" }
-  )
-}
-# creating public route table and to route table we need add peering connection and internet-gateway
-
-
-resource "aws_route_table_association" "public-rt-assoc" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public.*.id[count.index]
-  route_table_id = aws_route_table.public.id
-}
-# now we need to associate the Route-table with Sub-nets
-# we need to attach the route table to subnet manually
-# count refers the number of public subnets we created and it takes them all
-
-# TILL NOW WE CREATED A IGW AND TO THE TWO SUBNETS WE ASSOCIATED WITH PUBLIC ROUTE-TABLE THAT HAS IGW AND PEERING WITH WORKSTATION VPC
-
-
 resource "aws_eip" "ngw-eip" {
   vpc      = true
 }
 # we need to first create a elastic ip, to create nat-gateway
 
 
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.ngw-eip.id
-  subnet_id     = aws_subnet.public.*.id[0]
+#resource "aws_nat_gateway" "ngw" {
+#  allocation_id = aws_eip.ngw-eip.id
+#  subnet_id     = aws_subnet.public.*.id[0]
 
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-ngw" }
-  )
+#  tags = merge(
+#    local.common_tags,
+#    { Name = "${var.env}-ngw" }
+#  )
 
-  depends_on = [aws_internet_gateway.igw]
-}
+#  depends_on = [aws_internet_gateway.igw]
+#}
 # creating nat-gateway
-
-
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  }
-# peering the both vpc's
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.ngw.id
-  }
-# entry to private subnets will be through nat gateway
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-private-routetable" }
-  )
-}
-# creating private route table, so to point the natgateway to private subnets
-
-
-resource "aws_route_table_association" "private-rt-assoc" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private.*.id[count.index]
-  route_table_id = aws_route_table.private.id
-}
 
 
 resource "aws_route" "r" {
--- vars.tf
+++ vars.tf
@@ -1,9 +1,7 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "public_subnets_cidr" {}
-variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
-variable "availability_zones" {}
+
 # we are going to take a variable and give it
 # we have to send this cidr_block to roboshop-infra, main.tf
 # we are trying to maintain the uniformity of the variables also
