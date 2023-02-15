f03d779eb4bad34232a9879b78548bc8afa14068
raviteja <ravitejatfcdemo@gmail.com>
2023-02-15 10:01:28 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = merge(
     local.common_tags,
@@ -25,6 +26,7 @@ resource "aws_subnet" "private" {
   count = length(var.private_subnets_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = merge(
     local.common_tags,
--- vars.tf
+++ vars.tf
@@ -3,6 +3,7 @@ variable "env" {}
 variable "public_subnets_cidr" {}
 variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
+variable "availability_zones" {}
 # we are going to take a variable and give it
 # we have to send this cidr_block to roboshop-infra, main.tf
 # we are trying to maintain the uniformity of the variables also
