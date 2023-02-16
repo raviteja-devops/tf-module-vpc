875b4c800e05f76a4b852a5485033601f41a0845
raviteja <ravitejatfcdemo@gmail.com>
2023-02-16 11:15:40 +0530

    Terraform

--- subnets.tf
+++ subnets.tf
@@ -8,6 +8,9 @@ module "subnets" {
   for_each                  = var.subnets
   cidr_block                = each.value.cidr_block
   name                      = each.value.name
+  internet_gw               = lookup(each.value, "internet_gw", false)
+  nat_gw                    = lookup(each.value, "nat_gw", false)
+  create_nat_gw             = lookup(each.value, "create_nat_gw", false)
 
   vpc_id                    = aws_vpc.main.id
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -6,3 +6,6 @@ variable "default_vpc_id" {}
 variable "name" {}
 variable "vpc_peering_connection_id" {}
 variable "tags" {}
+variable "internet_gw" {}
+variable "nat_gw" {}
+variable "create_nat_gw" {}
