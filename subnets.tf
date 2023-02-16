e1109d681983908ce19678394c7a505eae23b586
raviteja <ravitejatfcdemo@gmail.com>
2023-02-16 13:52:35 +0530

    Terraform

--- subnets.tf
+++ subnets.tf
@@ -1,11 +1,30 @@
-module "subnets" {
-  source                    = "./subnets"
+#module "subnets" {
+#  source                    = "./subnets"
+#
+#  default_vpc_id            = var.default_vpc_id
+#  env                       = var.env
+#  availability_zone         = var.availability_zone
+#
+#  for_each                  = var.subnets
+#  cidr_block                = each.value.cidr_block
+#  name                      = each.value.name
+#  internet_gw               = lookup(each.value, "internet_gw", false)
+##  nat_gw                    = lookup(each.value, "nat_gw", false)
+#
+#  vpc_id                    = aws_vpc.main.id
+#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#  tags                      = local.common_tags
+#  gateway_id                = aws_internet_gateway.igw.id
+#}
+
+module "public_subnets" {
+  source = "./subnets"
 
   default_vpc_id            = var.default_vpc_id
   env                       = var.env
   availability_zone         = var.availability_zone
 
-  for_each                  = var.subnets
+  for_each                  = var.public_subnets
   cidr_block                = each.value.cidr_block
   name                      = each.value.name
   internet_gw               = lookup(each.value, "internet_gw", false)
--- vars.tf
+++ vars.tf
@@ -1,9 +1,6 @@
 variable "cidr_block" {}
 variable "env" {}
 variable "default_vpc_id" {}
-variable "subnets" {}
+variable "public_subnets" {}
+#variable "private_subnets" {}
 variable "availability_zone" {}
-
-# we are going to take a variable and give it
-# we have to send this cidr_block to roboshop-infra, main.tf
-# we are trying to maintain the uniformity of the variables also
