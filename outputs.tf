e0c23feec4ef0c17d4ca1a94b361b4945f2978fb
raviteja <ravitejatfcdemo@gmail.com>
2023-02-17 19:01:59 +0530

    Terraform

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,11 @@ output "vpc_peering_connection_id" {
   value = aws_vpc_peering_connection.peer.id
 }
 
-output "public_subnet_id" {
+output "public_subnet_ids" {
   value = module.public_subnets
 }
 
-output "private_subnet_id" {
+output "private_subnet_ids" {
   value = module.private_subnets
 }
 # we are taking the public subnet id's from the all subnet id's from subnets/output.tf
